56ef8f00c1a9dc6368f506ee3f32a4ef4cb73eb6
axelias <141047937+axelias@users.noreply.github.com>
2024-07-07 23:39:31 -0400

    Update inventario.py

--- inventario.py
+++ inventario.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import streamlit-aggrid
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 import os
 from babel.dates import format_date
 import locale
